9adaf5f4ee3b82e8f56b88098322b22245dd7bf5
edenchan@wawanesa.com <edenchan@wawanesa.com>
2021-07-01 21:39:07 -0400

    Use Typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,78 +1,42 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import Link from "next/link";
+import {
+  useColorMode,
+  Button,
+  Container,
+  Grid,
+  GridItem,
+  Box,
+  Stack,
+  VStack,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
 
 export default function Home() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Break Through</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Container>
+      <Grid templateColumns="repeat(2, 1fr)">
+        <GridItem>
+          <VStack>
+            <Heading as="h1" size="xl">
+              Find your Voice
+            </Heading>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Find your voice
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Through rap
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <Link
-            href="/hello"
-          >
-            <a
-              className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-            > <h3 className="text-2xl font-bold">Start Session &rarr;</h3>
-              <p className="mt-4 text-xl">
-                Start a rap session here!
-              </p></a>
-          </Link>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
+            <Text fontSize="xs">(xs) In love with React & Next</Text>
+            <Link href="/trainer">
+              <a>Start Session</a>
+            </Link>
+          </VStack>
+        </GridItem>
+        <GridItem>
+          <Button onClick={toggleColorMode}>
+            Toggle {colorMode === "light" ? "Dark" : "Light"}
+          </Button>
+          <p className="text-center">Insert Landing Page Here</p>
+        </GridItem>
+      </Grid>
+    </Container>
+  );
 }
