9e70cc0f8ec890e7f2168d0137be08de1a71eb77
edenchan@wawanesa.com <edenchan717@gmail.com>
2021-07-13 17:43:57 -0400

    Add chakra icons to landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,8 @@ import {
   Heading,
   Text, Flex,
 } from "@chakra-ui/react";
+import { ArrowForwardIcon } from '@chakra-ui/icons'
+
 
 import Image from 'next/image'
 import singer from '../public/singer.svg';
@@ -21,33 +23,35 @@ export default function Home() {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Container maxW="xl">
-      <Grid templateColumns="repeat(2, 1fr)">
-        <GridItem>
-          <VStack spacing="12px">
-            <Box>
-              <Heading as="h1" size="xl">
-                Find your Voice
-              </Heading>
-            </Box>
-            <Box>
-              <Text fontSize="md"> | About </Text>
-              <Text fontSize="xs"> Breakthrough empowers individuals to become more articulate through freestyle rap  </Text>
-            </Box>
-            <Link href="/trainer" passHref>
-              <Button>
-                Start Session
-              </Button>
-            </Link>
-          </VStack>
-        </GridItem>
-        <GridItem>
-          <Button onClick={toggleColorMode}>
-            Toggle {colorMode === "light" ? "Dark" : "Light"}
-          </Button>
-          <Image src={rapper} alt="Singer" />
-        </GridItem>
-      </Grid>
-    </Container>
+    <Grid templateColumns="repeat(2, 1fr)">
+      <GridItem colSpan={2}>
+        {/* Nav Bar */}
+        <Box w="100%" h='20vh' />
+      </GridItem>
+      <GridItem>
+        {/* Main Section */}
+        <Box h='60vh'>
+          <Box>
+            <Heading as="h1" >
+              Find your Voice
+            </Heading>
+          </Box>
+          <Box>
+            <Text fontSize="lg"> | About </Text>
+            <Text fontSize="md"> Breakthrough empowers individuals to become more articulate through freestyle rap  </Text>
+          </Box>
+          <Link href="/trainer" passHref>
+            <Button colorScheme='yellow'>Start Session <ArrowForwardIcon /></Button>
+          </Link>
+        </Box>
+      </GridItem>
+      <GridItem>
+        <Image src={rapper} alt="Singer" />
+      </GridItem>
+      <GridItem colSpan={2}>
+        {/* Footer */}
+        <Box h='20vh'></Box>
+      </GridItem>
+    </Grid>
   );
 }
