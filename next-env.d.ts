91b6c76fc3471d5688b55a65574b73ebbcf11231
kensonhui <asimplekevin@gmail.com>
2021-07-13 20:06:51 -0400

    Add type definition for mp3

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,8 +1,3 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
-
-declare module '*mp3' {
-    const src: string;
-    export default src;
-}
