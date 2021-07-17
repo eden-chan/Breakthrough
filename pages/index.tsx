ed8fafd0697a908274190fb296ae7aa4cfdacb89
resesiu <rebeccasiu123@hotmail.com>
2021-07-16 23:45:10 -0400

    e

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,49 +22,51 @@ import rapper from '../public/rapper.svg';
 
 export default function Home() {
   const { colorMode, toggleColorMode } = useColorMode();
-  
+
   return (
-      <Grid templateColumns="repeat(2, 1fr)">
-      
-        <GridItem colSpan={1}>
-          {/* Main Section */}
-          <Flex direction="column" h="60vh" w="100%" px="5rem">
-            <Box py="2rem">
-              <Heading as="h1" size="4xl">
-                Find your Voice
-              </Heading>
-            </Box>
-            <Box pb="2rem">
-              <Text fontSize="lg"> | About </Text>
-              <Text fontSize="md">
-                {' '}
-                Breakthrough empowers individuals to become more articulate by
-                developing their ability to freestyle rap{' '}
-              </Text>
-            </Box>
-            <Box>
-              <Link href="/trainer" passHref>
-                <Button
-                  borderRadius="md"
-                  bg="#FEBE02"
-                  color="#1A1A1A"
-                  h="100%"
-                >
-                  <Text fontSize="6xl">
-                    Start A Session <ArrowForwardIcon />
-                  </Text>{' '}
-                </Button>
-              </Link>
-            </Box>
-          </Flex>
-        </GridItem>
-        <GridItem colSpan={1}>
-          <Image src={rapper} alt="Singer" />
-        </GridItem>
-        <GridItem colSpan={2}>
-          {/* Footer */}
-          <Box h="20vh"></Box>
-        </GridItem>
-      </Grid>
+    <Grid templateColumns="repeat(2, 1fr)">
+
+      <GridItem colSpan={1}>
+        {/* Main Section */}
+        <Flex direction="column" h="60vh" w="100%" px="5rem">
+          <Box py="2rem">
+            <Heading as="h1" size="4xl">
+              Find your Voice
+            </Heading>
+          </Box>
+          <Box pb="2rem">
+            <Text fontSize="lg"> | About </Text>
+            <Text fontSize="md">
+              {' '}
+              Breakthrough empowers individuals to become more articulate by
+              developing their ability to freestyle rap{' '}
+            </Text>
+          </Box>
+          <Box>
+            <Link href="/trainer" passHref>
+
+              <Button
+                borderRadius="md"
+                bg="#FEBE02"
+                color="#1A1A1A"
+                h="150%"
+              >
+                <Text fontSize="2xl">
+                  Start A Session <ArrowForwardIcon />
+                </Text>{' '}
+              </Button>
+
+            </Link>
+          </Box>
+        </Flex>
+      </GridItem>
+      <GridItem colSpan={1}>
+        <Image src={rapper} alt="Singer" />
+      </GridItem>
+      <GridItem colSpan={2}>
+        {/* Footer */}
+        <Box h="20vh"></Box>
+      </GridItem>
+    </Grid>
   );
 }
